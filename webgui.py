a505f43e52080c530382ec6846b634d3e9bda64f
Pyplate <hitechgardener@gmail.com>
2014-03-03 14:38:44 +0000

    Update webgui.py

--- webgui.py
+++ webgui.py
@@ -43,7 +43,7 @@ def get_data(interval):
         curs.execute("SELECT * FROM temps")
     else:
 #        curs.execute("SELECT * FROM temps WHERE timestamp>datetime('now','-%s hours')" % interval)
-        curs.execute("SELECT * FROM temps WHERE timestamp>datetime('2013-09-21 21:30:02','-%s hours') AND timestamp<=datetime('2013-09-21 21:31:02')" % interval)
+        curs.execute("SELECT * FROM temps WHERE timestamp>datetime('2013-09-19 21:30:02','-%s hours') AND timestamp<=datetime('2013-09-19 21:31:02')" % interval)
 
     rows=curs.fetchall()
 
@@ -114,15 +114,15 @@ def show_stats(option):
     if option is None:
         option = str(24)
 
-    curs.execute("SELECT timestamp,max(temp) FROM temps WHERE timestamp>datetime('2013-09-21 21:30:02','-%s hour') AND timestamp<=datetime('2013-09-21 21:31:02')" % option)
+    curs.execute("SELECT timestamp,max(temp) FROM temps WHERE timestamp>datetime('2013-09-19 21:30:02','-%s hour') AND timestamp<=datetime('2013-09-19 21:31:02')" % option)
     rowmax=curs.fetchone()
     rowstrmax="{0}&nbsp&nbsp&nbsp{1}C".format(str(rowmax[0]),str(rowmax[1]))
 
-    curs.execute("SELECT timestamp,min(temp) FROM temps WHERE timestamp>datetime('2013-09-21 21:30:02','-%s hour') AND timestamp<=datetime('2013-09-21 21:31:02')" % option)
+    curs.execute("SELECT timestamp,min(temp) FROM temps WHERE timestamp>datetime('2013-09-19 21:30:02','-%s hour') AND timestamp<=datetime('2013-09-19 21:31:02')" % option)
     rowmin=curs.fetchone()
     rowstrmin="{0}&nbsp&nbsp&nbsp{1}C".format(str(rowmin[0]),str(rowmin[1]))
 
-    curs.execute("SELECT avg(temp) FROM temps WHERE timestamp>datetime('2013-09-21 21:30:02','-%s hour') AND timestamp<=datetime('2013-09-21 21:31:02')" % option)
+    curs.execute("SELECT avg(temp) FROM temps WHERE timestamp>datetime('2013-09-19 21:30:02','-%s hour') AND timestamp<=datetime('2013-09-19 21:31:02')" % option)
     rowavg=curs.fetchone()
 
 
@@ -142,7 +142,7 @@ def show_stats(option):
     print "<table>"
     print "<tr><td><strong>Date/Time</strong></td><td><strong>Temperature</strong></td></tr>"
 
-    rows=curs.execute("SELECT * FROM temps WHERE timestamp>datetime('2013-09-21 21:30:02','-1 hour') AND timestamp<=datetime('2013-09-21 21:31:02')")
+    rows=curs.execute("SELECT * FROM temps WHERE timestamp>datetime('2013-09-19 21:30:02','-1 hour') AND timestamp<=datetime('2013-09-19 21:31:02')")
     for row in rows:
         rowstr="<tr><td>{0}&emsp;&emsp;</td><td>{1}C</td></tr>".format(str(row[0]),str(row[1]))
         print rowstr
